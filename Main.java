56606fe66f975ebb74b3f0ece5bcd6ed31e090da
Vildan16 <64701909+Vildan16@users.noreply.github.com>
2022-09-10 08:51:46 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,110 +1,97 @@
 package tictactoe;
+
 import java.util.Scanner;
 
 public class Main {
-
-    public static boolean checkWinner(char[][] str, char c) {
-        if (str[0][0] == c && str[0][1] == c && str[0][2] == c)
-            return true;
-        if (str[1][0] == c && str[1][1] == c && str[1][2] == c)
-            return true;
-        if (str[2][0] == c && str[2][1] == c && str[2][2] == c)
-            return true;
-        if (str[0][0] == c && str[1][0] == c && str[2][0] == c)
-            return true;
-        if (str[0][1] == c && str[1][1] == c && str[2][1] == c)
-            return true;
-        if (str[0][2] == c && str[1][2] == c && str[2][2] == c)
-            return true;
-        if (str[0][0] == c && str[1][1] == c && str[2][2] == c)
-            return true;
-        if (str[0][2] == c && str[1][1] == c && str[2][0] == c)
-            return true;
-        return false;
-    }
-
-    public static void printGrid(char[][] str) {
-        System.out.println("---------");
-        System.out.println("| " + str[0][0] + " " + str[0][1] + " " + str[0][2] + " |");
-        System.out.println("| " + str[1][0] + " " + str[1][1] + " " + str[1][2] + " |");
-        System.out.println("| " + str[2][0] + " " + str[2][1] + " " + str[2][2] + " |");
-        System.out.println("---------");
-    }
-
-    public static boolean checkStatus(char[][] str) {
-        int numX = 0;
-        int numO = 0;
-        boolean winX = checkWinner(str, 'X');
-        boolean winO = checkWinner(str, 'O');
-        for (int i = 0; i < str.length; i++) {
-            for (int j = 0; j < str[i].length; j++) {
-                if (str[i][j] == 'X')
-                    numX++;
-                if (str[i][j] == 'O')
-                    numO++;
-            }
-        }
-        if (!winX && !winO && numX + numO == 9) {
-            System.out.println("Draw");
-            return true;
-        }
-        else if (winX) {
-            System.out.println("X wins");
-            return true;
-        }
-        else if (winO) {
-            System.out.println("O wins");
-            return true;
-        }
-        return false;
-    }
-
-    public static void makeMove(char[][] grid, char c) {
-        Scanner scanner = new Scanner(System.in);
-        int newX;
-        int newY;
-        while (true) {
-            System.out.print("Enter the coordinates: ");
-            if (scanner.hasNextInt())
-                newX = scanner.nextInt();
-            else {
-                System.out.println("You should enter numbers!");
-                continue;
-            }
-            if (scanner.hasNextInt())
-                newY = scanner.nextInt();
-            else {
-                System.out.println("You should enter numbers!");
-                continue;
-            }
-            if (newX < 1 || newX > 3 || newY < 1 || newY > 3) {
-                System.out.println("Coordinates should be from 1 to 3!");
-                continue;
-            } else if (grid[newX - 1][newY - 1] != ' ') {
-                System.out.println("This cell is occupied! Choose another one!");
-                continue;
-            }
-            grid[newX - 1][newY - 1] = c;
-            printGrid(grid);
-            break;
-        }
-    }
+    private static char[][] grid;
 
     public static void main(String[] args) {
-        char[][] grid = new char[3][3];
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                grid[i][j] = ' ';
+        String gridString = "         ";
+        char c = 'X';
+        initGrid(gridString);
+        printGrid();
+        while (true) {
+            c = c == 'X' ? 'O' : 'X';
+            takeMove(c);
+            printGrid();
+            String result = getResult();
+            if (result != null) {
+                System.out.println(result);
+                break;
             }
         }
-        printGrid(grid);
-        while (true) {
-            makeMove(grid, 'X');
-            if (checkStatus(grid))
-                break;
-            makeMove(grid, 'O');
-            if (checkStatus(grid))
-                break;
+    }
+
+    private static void takeMove(char c) {
+        Scanner scanner = new Scanner(System.in);
+        char xChar = scanner.next().charAt(0);
+        char yChar = scanner.next().charAt(0);
+
+        if (!Character.isDigit(xChar) || !Character.isDigit(yChar)) {
+            System.out.println("You should enter numbers!");
+            takeMove(c);
+            return;
+        } else if (xChar < '1' || xChar > '3' || yChar < '1' || yChar > '3') {
+            System.out.println("Coordinates should be from 1 to 3!");
+            takeMove(c);
+            return;
         }
+        int x = Integer.parseInt(String.valueOf(xChar));
+        int y = Integer.parseInt(String.valueOf(yChar));
+
+        if (grid[x - 1][y - 1] != ' ') {
+            System.out.println("This cell is occupied! Choose another one!");
+            takeMove(c);
+        }
+        else
+            grid[x - 1][y - 1] = c;
+    }
+
+    private static void initGrid(String gridString) {
+        grid = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                grid[i][j] = gridString.charAt(i * 3 + j);
+            }
+        }
+    }
+
+    private static void printGrid() {
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (j == 0 || j == 4)
+                    System.out.print("|");
+                else
+                    System.out.print(grid[i][j - 1]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println("---------");
+    }
+
+    private static String getResult() {
+        String result = null;
+        String gridString = String.valueOf(grid[0]) + String.valueOf(grid[1]) + String.valueOf(grid[2]);
+
+        if (isWinner(gridString, 'X'))
+            result = "X wins";
+        else if (isWinner(gridString, 'O'))
+            result = "O wins";
+        else if (!gridString.contains(" "))
+            result = "Draw";
+        return result;
+    }
+
+    private static boolean isWinner(String grid, char x) {
+        return  (grid.charAt(0) == x && grid.charAt(1) == x && grid.charAt(2) == x ||
+                (grid.charAt(3) == x && grid.charAt(4) == x && grid.charAt(5) == x) || 
+                (grid.charAt(6) == x && grid.charAt(7) == x && grid.charAt(8) == x) ||
+                (grid.charAt(0) == x && grid.charAt(3) == x && grid.charAt(6) == x) ||
+                (grid.charAt(1) == x && grid.charAt(4) == x && grid.charAt(7) == x) ||
+                (grid.charAt(2) == x && grid.charAt(5) == x && grid.charAt(8) == x) ||
+                (grid.charAt(0) == x && grid.charAt(4) == x && grid.charAt(5) == x) ||
+                (grid.charAt(2) == x && grid.charAt(4) == x && grid.charAt(6) == x));
     }
 }
